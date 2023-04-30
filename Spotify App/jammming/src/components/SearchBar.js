477b12d17bf3e7368cabdd658799171c657e60e2
InbalGam <inbalgam@post.bgu.ac.il>
2023-04-30 14:55:42 +0300

    started static components

---
+++ Spotify App/jammming/src/components/SearchBar.js
@@ -0,0 +1,11 @@
+function SearchBar() {
+    return (
+        <div className="search-bar">
+            <label for='search'>Search for music </label>
+            <input id='search' type='text' />
+            <input type="submit" value="Submit"/>
+        </div>
+    );
+};
+
+export default SearchBar;
---
+++ Spotify App/jammming/src/components/SearchResults.js
@@ -0,0 +1,13 @@
+function Results() {
+    return (
+        <div className="search-results">
+            <h2>Results</h2>
+            <ul>
+                <li>1.</li>
+                <li>2.</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Results;
