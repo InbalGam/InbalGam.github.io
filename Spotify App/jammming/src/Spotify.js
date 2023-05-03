4d1671af8f2a58145d5892534d92365665f967bf
InbalGam <inbalgam@post.bgu.ac.il>
2023-05-03 11:07:19 +0300

    replaced Buffer with authentication var

--- Spotify App/jammming/src/Spotify.js
+++ Spotify App/jammming/src/Spotify.js
@@ -1,11 +1,12 @@
-var client_id = '0206e171414f4fe28aa42635cb40721e';
-var client_secret = 'fbf0ada5951440fa9c588c45d23dc81e';
+const authorizationVar = 'MDIwNmUxNzE0MTRmNGZlMjhhYTQyNjM1Y2I0MDcyMWU6ZmJmMGFkYTU5NTE0NDBmYTljNTg4YzQ1ZDIzZGM4MWU='
+// var client_id = '0206e171414f4fe28aa42635cb40721e';
+// var client_secret = 'fbf0ada5951440fa9c588c45d23dc81e';
 const url = 'https://accounts.spotify.com/api/token';
 
 
 async function getAccessToken() {
     const response = await fetch(url, {method: 'POST',
-                                       headers: {'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64')),
+                                       headers: {'Authorization': 'Basic ' + authorizationVar,
                                                 'Content-Type': "application/x-www-form-urlencoded"},
                                        body: 'grant_type=client_credentials'});
     const jsonData = await response.json();
@@ -22,5 +23,5 @@ async function searchSpotify(query) {
     const jsonData = await response.json();
     return jsonData;
 };
-
+//searchSpotify('hello');
 export default searchSpotify;
