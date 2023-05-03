c87a60dd55b3d81c63ff0577dbcd35b751ebfa36
InbalGam <inbalgam@post.bgu.ac.il>
2023-05-03 08:42:26 +0300

    added the option to rename the playlist

--- Spotify App/jammming/src/components/Playlist.js
+++ Spotify App/jammming/src/components/Playlist.js
@@ -1,9 +1,17 @@
 import Tracklist from "./Tracklist";
+import { useState } from "react";
 
 function Playlist(props) {
+    const [text, setText] = useState('');
+    function handleTextChange(e) {
+        setText(e.target.value);
+    };
+
+
     return (
         <div className="playlist">
-            <h2>Playlist</h2>
+            <label htmlFor="playlistName">Playlist</label>
+            <input id='playlistName' type='text' name='playlist' value={text} onChange={handleTextChange} />
             <ul>
             <Tracklist tracks={props.tracks} button={'-'} onButtonClick={props.onButtonClick} />
             </ul>
